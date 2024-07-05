64c92822b68beedd70b83695dac6c83b3e3ed361
Aris Wildan J <80802630+ariswj29@users.noreply.github.com>
2024-07-05 13:08:15 +0700

    feat: add configuration tailwindcss & make ui navbar (#6)

---
+++ apps/web/src/data/data.js
@@ -0,0 +1,17 @@
+export const navbars = [
+  {
+    id: 1,
+    title: 'Home',
+    link: '/',
+  },
+  {
+    id: 2,
+    title: 'Login',
+    link: '/login',
+  },
+  {
+    id: 3,
+    title: 'Register',
+    link: '/register',
+  },
+];
