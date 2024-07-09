259190b83c3368019bc753f646e352da7b2b5a43
andisittirahmah <asrahmah00@gmail.com>
2024-07-09 17:33:26 +0700

    feat: add handleclick in categories icon

--- apps/web/src/components/homeSection/Category.tsx
+++ apps/web/src/components/homeSection/Category.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 const categories = [
@@ -11,11 +12,23 @@ const categories = [
 ];
 
 const Category = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleClick = (index: any) => {
+    setSelectedCategory(index);
+  };
   return (
     <div className="grid grid-cols-3 md:grid-cols-6 justify-around items-center px-4 py-20">
       {categories.map((category, index) => (
-        <div key={index}>
-          <div className="w-20 h-20 mb-2 mx-auto border-2 border-secondary rounded-full flex items-center justify-center p-2">
+        <div key={index} className="hover:text-blue-900 hover:font-semibold">
+          <div
+            className={`w-20 h-20 mb-2 mx-auto border-2 border-secondary rounded-full flex items-center justify-center p-2 cursor-pointer transition duration-300 ease-in-out transform hover:bg-blue-900 ${
+              selectedCategory === index
+                ? 'border-blue-800'
+                : 'border-secondary'
+            }`}
+            onClick={() => handleClick(index)}
+          >
             <Image
               src={category.icon}
               alt={category.name}
@@ -23,7 +36,7 @@ const Category = () => {
               height={50}
             />
           </div>
-          <div className="text-sm text-center m-2">{category.name}</div>
+          <div className="text-sm text-center m-2 ">{category.name}</div>
         </div>
       ))}
     </div>
