ec0cde7713437d072d5e386c3471a01df44ab840
Aris Wildan J <80802630+ariswj29@users.noreply.github.com>
2024-07-09 13:10:01 +0700

    feat: add feature using referral code + 10000 points & handle check referral code (#18)
    * feat: add configuration tailwindcss & make ui navbar
    
    * feat: create ui login page
    
    * feat: change bg navbar
    
    * refactor: hide .env file
    
    * refactor: hide .env file
    
    * refactor: hide .env file
    
    * feat: create function login & register
    
    * feat: add button to show password & input referral code
    
    * feat: functionality ui register
    
    * feat: add feature using referral code + 10000 points & handle check referral code

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -27,7 +27,10 @@ export const register = async (req: Request, res: Response) => {
       },
     });
     if (existingUser) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.json({
+        status: 'error',
+        message: 'User already exists',
+      });
     }
     const salt = await genSalt(10);
     const hashedPassword = await hash(password, salt);
@@ -41,7 +44,36 @@ export const register = async (req: Request, res: Response) => {
         usingReferralCode,
       },
     });
-    res.status(201).json({ message: 'success', data: user });
+
+    const profile = await prisma.profile.create({
+      data: {
+        userId: user.id,
+        referralCode: generateReferralCode(8),
+        points: 0,
+        discount: 0,
+      },
+    });
+
+    const usingReferralCodeProfile = await prisma.profile.findFirst({
+      where: {
+        referralCode: usingReferralCode,
+      },
+    });
+
+    await prisma.profile.update({
+      where: {
+        id: usingReferralCodeProfile?.id,
+      },
+      data: {
+        points: usingReferralCodeProfile?.points! + 10000,
+      },
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'You have successfully registered',
+      data: user,
+    });
   } catch (error) {
     res.status(400).json({ error: 'error' });
   }
@@ -92,3 +124,36 @@ export const login = async (req: Request, res: Response) => {
     res.status(400).json({ error: 'error' });
   }
 };
+
+export const checkReferralCode = async (req: Request, res: Response) => {
+  try {
+    const { usingReferralCode } = req.body;
+    const user = await prisma.profile.findFirst({
+      where: {
+        referralCode: usingReferralCode,
+      },
+    });
+    if (!user) {
+      return res.json({
+        status: 'error',
+        message: 'Invalid referral code',
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      message: 'Referral code is valid',
+    });
+  } catch (error) {
+    res.status(400).json({ error: 'error' });
+  }
+};
+
+function generateReferralCode(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
