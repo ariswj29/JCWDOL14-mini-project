7a5d9c0d9365e460b96cfcd7afbb14acb9baee4b
andisittirahmah <asrahmah00@gmail.com>
2024-07-14 10:58:16 +0700

    feat: delete file photo

--- apps/web/src/api/event.tsx
+++ apps/web/src/api/event.tsx
@@ -4,8 +4,14 @@ import axios from 'axios';
 
 const base_url_api = 'http://localhost:8000/api';
 
-export async function getAllEvents(data: any) {
+export async function getAllEvents(data: {}) {
   const url = base_url_api + '/events';
+  const res = await axios.get(url, data);
+
+  return res.data;
+}
+export async function getEvent(id: Number, data: {}) {
+  const url = base_url_api + '/events' + id;
   const res = await axios.get(url, data);
 
   return res.data;
