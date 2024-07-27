2ed1f2deaee096d4385d8b752b61562176bb3e86
andisittirahmah <asrahmah00@gmail.com>
2024-07-27 23:07:43 +0700

    feat: create input promotion and validation

--- apps/api/src/controllers/review.controller.ts
+++ apps/api/src/controllers/review.controller.ts
@@ -1,11 +1,14 @@
+import { reviewSchema } from '@/schema/schema';
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 const prisma = new PrismaClient();
 
 export async function createReview(req: Request, res: Response) {
-  const { rating, comment, eventId, transactionId, userId } = req.body;
   try {
+    await reviewSchema.validate(req.body, { abortEarly: false });
+    const { rating, comment, eventId, transactionId, userId } = req.body;
     const review = await prisma.review.create({
       data: {
         rating,
@@ -16,12 +19,18 @@ export async function createReview(req: Request, res: Response) {
       },
     });
     res.status(200).json({
-      status: 'succeess',
+      status: 'success',
       message: 'success create review',
       data: review,
     });
   } catch (error) {
-    res.status(500).json({ message: 'something went wrong' });
+    if (error instanceof yup.ValidationError) {
+      return res.status(400).json({
+        status: 'error',
+        message: error.errors,
+      });
+    }
+    res.status(400).json({ message: 'something went wrong' });
   }
 }
 
@@ -58,6 +67,6 @@ export const getAllReview = async (req: Request, res: Response) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ error: 'error' });
+    res.status(400).json({ error: 'error' });
   }
 };
---
+++ apps/web/src/app/[id]/review/page.tsx
@@ -0,0 +1,166 @@
+'use client';
+
+import { getEvent } from '@/api/event';
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+import { id as idLocale } from 'date-fns/locale';
+import { Event } from '@/interface/interface';
+import { useRouter } from 'next/navigation';
+import { getProfileProcess } from '@/api/profile';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { reviewSchema } from '@/schema/schema';
+import { createReview } from '@/api/review';
+import CardOrdered from '@/components/CardOrdered';
+
+export default function ReviewPage(context: any) {
+  const { params } = context;
+  const router = useRouter();
+  const [profile, setProfile] = useState({
+    id: 0,
+    saldo: 0,
+    points: 0,
+    discount: 0,
+    referralCode: '',
+  });
+  const [user, setUser] = useState({
+    rating: '',
+    comment: '',
+  });
+  const [event, setEvent] = useState<Event | null>(null);
+  const [totalTransaction, setTotalTransaction] = useState(0);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(reviewSchema),
+  });
+
+  useEffect(() => {
+    const fetchEventDetails = async () => {
+      try {
+        const user = localStorage.getItem('user');
+        if (user) {
+          const { profileId } = JSON.parse(user);
+          const response = await getProfileProcess(profileId);
+
+          setUser(response.data.user);
+        } else {
+          console.error('User not found');
+        }
+
+        const response = await getEvent(params.id);
+        setEvent(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (params?.id) {
+      fetchEventDetails();
+    }
+  }, [params?.id, router]);
+
+  const handleTotalTransactionChange = (total: number) => {
+    setTotalTransaction(total);
+  };
+
+  const formSubmit = async (formData: any) => {
+    try {
+      const user = localStorage.getItem('user');
+      if (user) {
+        const { profileId } = JSON.parse(user);
+        const response = await createReview({
+          userId: profileId,
+          eventId: event?.id,
+          ...formData,
+        });
+        console.log('response', response);
+        if (response.status === 'success') {
+          alert('Success create review');
+          router.push(`/${event?.id}/review`);
+        } else {
+          alert('Failed create review');
+        }
+      }
+    } catch (error) {
+      console.error('Error creating/updating promotion:', error);
+    }
+  };
+
+  if (!event) {
+    return <div>Loading...</div>;
+  }
+
+  const formattedDate = format(new Date(event.date), 'd MMMM yyyy', {
+    locale: idLocale,
+  });
+
+  return (
+    <div className="container max-w-screen-xl mx-auto items-center p-12">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+        <div className="md:col-span-2 order-2 md:order-1">
+          <div className="bg-white shadow-md rounded-lg p-6">
+            <h3 className="text-2xl font-bold text-gray-800">Review Ordered</h3>
+            <form onSubmit={handleSubmit(formSubmit)}>
+              <div className="md:grid gap-4">
+                <div>
+                  <label htmlFor="rating" className="text-base font-medium">
+                    Rating
+                  </label>
+                  <select className="w-full border p-2" {...register('rating')}>
+                    <option value="" disabled selected>
+                      select your rating
+                    </option>
+                    <option>⭐️⭐️⭐️⭐️⭐️</option>
+                    <option>⭐️⭐️⭐️⭐️</option>
+                    <option>⭐️⭐️⭐️</option>
+                    <option>⭐️⭐️</option>
+                    <option>⭐️</option>
+                  </select>
+                  {errors.rating && (
+                    <p className="text-sm text-red-500">
+                      {errors.rating.message}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor="comment" className="text-base font-medium">
+                    Comment
+                  </label>
+                  <input
+                    type="text"
+                    id="comment"
+                    placeholder="Please leave a review or comment "
+                    className="w-full p-2 border border-gray-300 rounded-md"
+                    {...register('comment')}
+                    defaultValue={user.comment}
+                  />
+                  <span className="text-red-500">
+                    {errors.comment?.message}
+                  </span>
+                </div>
+                <div className="formData my-4">
+                  <input
+                    className="bg-secondary px-4 py-2 cursor-pointer hover:font-bold rounded"
+                    type="submit"
+                    value="Submit"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <CardOrdered
+          profile={profile}
+          event={event}
+          date={formattedDate}
+          onTotalTransactionChange={handleTotalTransactionChange}
+        />
+      </div>
+    </div>
+  );
+}
